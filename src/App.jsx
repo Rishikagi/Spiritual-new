da3052d4541b2e464deb5d631773b71973fe793b
Rishika <rishika@example.com>
2025-06-27 17:43:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import Footer from './Footer.jsx';
+import Events from './Events.jsx';
+
+import './App.css'
+import Portfolio from './Portfolio.jsx';
+import Achievements from './Achievements.jsx';
+
+function App() {
+ 
+
+  return (
+    <>
+     <section className='footer bg-[#222222]'><Footer/></section>
+     <section className='portfolio bg-white'><Portfolio/></section>
+     <section className='acheivements '><Achievements/></section>
+     <section className='events bg-white'><Events/></section>
+    </>
+  )
+}
+
+export default App
